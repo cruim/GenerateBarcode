4d94ac4a5bd6459a2eb6998a800c62117a0388d1
Arashkevich <hedgehog147@gmail.com>
2016-04-28 14:30:08 +0300

    init commit

---
+++ Generate_Barcode/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Generate_Barcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.btnGenerate = new System.Windows.Forms.Button();
+			this.txtBox = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.btnExit = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnGenerate
+			// 
+			this.btnGenerate.Location = new System.Drawing.Point(587, 12);
+			this.btnGenerate.Name = "btnGenerate";
+			this.btnGenerate.Size = new System.Drawing.Size(140, 42);
+			this.btnGenerate.TabIndex = 0;
+			this.btnGenerate.Text = "Generate";
+			this.btnGenerate.UseVisualStyleBackColor = true;
+			this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+			// 
+			// txtBox
+			// 
+			this.txtBox.Location = new System.Drawing.Point(12, 12);
+			this.txtBox.Multiline = true;
+			this.txtBox.Name = "txtBox";
+			this.txtBox.Size = new System.Drawing.Size(537, 42);
+			this.txtBox.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 79);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(833, 144);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(763, 12);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 41);
+			this.btnExit.TabIndex = 3;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(857, 246);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.txtBox);
+			this.Controls.Add(this.btnGenerate);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Generate_Barcode/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace Generate_Barcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string barcode = txtBox.Text;
+
+            Bitmap bitmap = new Bitmap(barcode.Length * 40, 150);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                Font font = new System.Drawing.Font("idautomationhc39m", 20);
+                PointF point = new PointF(2f, 2f);
+                SolidBrush black = new SolidBrush(Color.Black);
+                SolidBrush white = new SolidBrush(Color.White);
+                graphics.FillRectangle(white, 0, 0, bitmap.Width, bitmap.Height);
+                graphics.DrawString("*" + barcode + "*", font, black, point);
+            }
+            using(MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                pictureBox1.Image = bitmap;
+                pictureBox1.Height = bitmap.Height;
+                pictureBox1.Width = bitmap.Width;
+            }
+        }
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+        
+    }
+}
